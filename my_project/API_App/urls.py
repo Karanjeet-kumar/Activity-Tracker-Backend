6bb9827711865db5702e21e98a541e8f284a9058
Karanjeet-kumar <karanrajput8400@gmail.com>
2025-06-14 01:30:55 +0530

    VerifyActivityListView-API-Created

--- my_project/API_App/urls.py
+++ my_project/API_App/urls.py
@@ -1,7 +1,7 @@
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path
 from API_App import views
-from .views import AssignedActivityListView, AssignedTaskListView, LogoutAPIView, TrnActivityCreateView, TrnActivityListView, TrnActivityTaskCreateView, TrnTaskUpdateCreateView, UpdateActivityAcceptanceView, UserListAPIView, VerifierUserListAPIView
+from .views import AssignedActivityListView, AssignedTaskListView, LogoutAPIView, TrnActivityCreateView, TrnActivityListView, TrnActivityTaskCreateView, TrnTaskUpdateCreateView, UpdateActivityAcceptanceView, UserListAPIView, VerifierUserListAPIView, VerifyActivityListView
 
 urlpatterns = [
     # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -21,5 +21,6 @@ urlpatterns = [
     path('add/task/', TrnActivityTaskCreateView.as_view(), name='add-task'),
     path('tasks/<int:user_id>/', AssignedTaskListView.as_view(), name='tasks-list'),
     path('add/task-updates/', TrnTaskUpdateCreateView.as_view(), name='add-task-update'),
+    path('get/verifiers/activities/<int:user_id>/', VerifyActivityListView.as_view(), name='get-verify-activities'),
 ]
 
